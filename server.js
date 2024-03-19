d056d6cacc8f536fa9c26f6447beae468fc11e9f
meni-kr <meni.kramer@gmail.com>
2024-03-19 15:58:00 +0200

    Add logger and cookies

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+
 import { bugService } from './services/bug.service.js'
+import { loggerService } from './services/logger.service.js'
 
 const app = express()
 
 app.use(express.static('public'))
- 
+app.use(cookieParser())
 
-app.get('/', (req, res) => res.send('my first server'))
+// app.get('/', (req, res) => res.send('my first server'))
 
 // get bugs
 app.get('/api/bug', (req, res) => {
@@ -15,13 +18,16 @@ app.get('/api/bug', (req, res) => {
             res.send(bugs)
         })
         .catch(err => {
-            // loggerService.error('Cannot get bugs', err)
+            loggerService.error('Cannot get bugs', err)
             res.status(400).send('Cannot get bugs')
         })
 })
 
-// Get bug (READ)
+// save / create
 app.get('/api/bug/save', (req, res) => {
+
+    loggerService.debug('req.query', req.query)
+
     const bugToSave = {
         title: req.query.title,
         severity: +req.query.severity,
@@ -32,17 +38,28 @@ app.get('/api/bug/save', (req, res) => {
     bugService.save(bugToSave)
         .then(bug => res.send(bug))
         .catch((err) => {
-            // loggerService.error('Cannot save bug', err)
+            loggerService.error('Cannot save bug', err)
             res.status(400).send('Cannot save bug')
         })
 })
 
 app.get('/api/bug/:bugId', (req, res) => {
     const bugId = req.params.bugId
+
+    const { visitedBugs = [] } = req.cookies 
+
+
+    if (!visitedBugs.includes(bugId)) {
+        if (visitedBugs.length >= 3) return res.status(401).send('Wait for a bit')
+        else visitedBugs.push(bugId)
+    }
+    
+    res.cookie('visitedBugs', visitedBugs, { maxAge: 1000 * 70 })
+
     bugService.getById(bugId)
         .then(bug => res.send(bug))
         .catch(err => {
-            // loggerService.error(err)
+            loggerService.error(err)
             res.status(400).send('Cannot get bug')
         })
 })
@@ -51,13 +68,19 @@ app.get('/api/bug/:bugId/remove', (req, res) => {
     console.log('delete....');
     const bugId = req.params.bugId
     bugService.remove(bugId)
-        .then(() => res.send(bugId))
+        .then(() => {
+            loggerService.info(`Bug ${bugId} removed`)
+            res.send('Removed')
+        })
         .catch((err) => {
-            // loggerService.error('Cannot remove bug', err)
+            loggerService.error('Cannot remove bug', err)
             res.status(400).send('Cannot remove bug')
         })
 })
 
 
-app.listen(3030, () => console.log('Server ready at port: 3030'))
+const PORT = 3030
+app.listen(PORT, () =>
+    loggerService.info(`Server listening on port http://127.0.0.1:${PORT}/`)
+)
 
